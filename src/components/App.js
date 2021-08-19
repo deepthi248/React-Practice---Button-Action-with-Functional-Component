5f3ebfd620d493b4cd6275fdb0f0ac3d8c157da7
deepthi <deepthibhogaraju.3@gmail.com>
2021-08-19 17:15:49 +0530

    problem solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
+
 function App() {
+  const [para, renderPara] = useState('');
+  const pHidden = () => {
+    renderPara({ para: true })
+  }
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click"
+        onClick={pHidden}
+      >show para</button>
+      {para ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool.
+          This makes me so happy
+        </p>
+      ) : null}
     </div>
   );
 }
